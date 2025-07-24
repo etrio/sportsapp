d8014e83aee2bd0acb646d476fb9c10ef20da517
Maks Dudek <m.dudek@zsek.olsztyn.pl>
2025-07-25 01:30:54 +0200

    Initial push

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from 'react-native';
+
+export default function TabOneScreen() {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+      <View>
+        <Text className='text-red-700'>feed</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
